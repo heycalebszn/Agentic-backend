9b977ef1a855ab5c36b9dfc91b6b8e92c757c6a2
caleb <kalejaiyecaleb@gmail.com>
2025-02-27 01:25:52 +0100

    db

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://kalejaiyecaleb:BsnOxjMIJj5fkC2P@cluster0.4dxxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
